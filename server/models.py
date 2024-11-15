45292a5463f86c971f65baf7be5c5d3c72aa43ff
Guillermo <vital_tmv@yahoo.com>
2024-11-15 15:38:54 -0600

    correct relationships between models

--- server/models.py
+++ server/models.py
@@ -4,42 +4,47 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 from sqlalchemy.orm import validates
 
-# Models go here!
+
+
 class Song(db.Model, SerializerMixin):
     __tablename__ = 'songs'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    artist = db.Column(db.String)
+    title = db.Column(db.String, nullable=False)
+    artist = db.Column(db.String, nullable=False)
     genre = db.Column(db.String)
     duration = db.Column(db.Time)
 
-    def __repr__(self):
-        return f'<Song Title:{self.title}, Artist:{self.artist}, Genre:{self.genre}, Duration:{self.duration}>'
+    # Direct access to Playlist_song association
+    playlist_songs = db.relationship('Playlist_song', back_populates='song', cascade='all, delete-orphan')
 
+    def __repr__(self):
+        return f'<Song Title:{self.title}, Artist:{self.artist}>'
 
 class Playlist_song(db.Model, SerializerMixin):
     __tablename__ = 'playlist_songs'
 
     id = db.Column(db.Integer, primary_key=True)
+    song_id = db.Column(db.Integer, db.ForeignKey('songs.id'), nullable=False)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlists.id'), nullable=False)
+    explicit = db.Column(db.Boolean, default=False)
 
-    song_id = db.Column(db.Integer, db.ForeignKey('songs.id'))
-    playlist_id = db.Column(db.Integer, db.ForeignKey('playlists.id'))
+    # Relationships to Song and Playlist
+    song = db.relationship('Song', back_populates='playlist_songs')
+    playlist = db.relationship('Playlist', back_populates='playlist_songs')
 
-
-    explicit = db.Column(db.Boolean, nullable=False, default=False)
-   
     def __repr__(self):
-        return f'<Playlist_song Explicit:{self.explicit}>'
-    
+        return f'<Playlist_song SongID:{self.song_id}, PlaylistID:{self.playlist_id}, Explicit:{self.explicit}>'
 
 class Playlist(db.Model, SerializerMixin):
     __tablename__ = 'playlists'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
     description = db.Column(db.String)
 
+    # Direct access to Playlist_song association
+    playlist_songs = db.relationship('Playlist_song', back_populates='playlist', cascade='all, delete-orphan')
 
     def __repr__(self):
         return f'<Playlist Name:{self.name}, Description:{self.description}>'
